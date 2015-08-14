a2032b5c1726b1a81fafddbb40f2285e8f47ccbf
Udit Raikwar <udit043@users.noreply.github.com>
2015-08-14 13:13:22 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <string.h>
+
+void mainmenu();
+int main()
+{
+    system("Title Automated Time Table Generator");
+    clrscr();system("cls");
+    textcolor(15);
+    mainmenu();
+}
